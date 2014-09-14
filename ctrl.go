99adfc5da7d2f2dd82df831e190c0875f9465fce
Jeremy Saenz <jeremy.saenz@gmail.com>
2014-09-14 16:50:23 -0700

    Initial commit. Just playing with some ideas

---
+++ ctrl.go
@@ -0,0 +1,77 @@
+package ctrl
+
+import (
+	"errors"
+	"net/http"
+	"reflect"
+)
+
+var (
+	interfaceType = interfaceOf((*Controller)(nil))
+)
+
+type Controller interface {
+	Init(http.ResponseWriter, *http.Request)
+	Destroy()
+}
+
+type Base struct {
+	Request        *http.Request
+	ResponseWriter http.ResponseWriter
+}
+
+func (b *Base) Init(rw http.ResponseWriter, r *http.Request) {
+	b.Request, b.ResponseWriter = r, rw
+}
+
+func (b *Base) Destroy() {
+}
+
+func Action(action interface{}) http.Handler {
+	val := reflect.ValueOf(action)
+	t, err := controllerType(val)
+	if err != nil {
+		panic(err)
+	}
+
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		v := reflect.New(t)
+		c := v.Interface().(Controller)
+		c.Init(rw, r)
+		val.Call([]reflect.Value{v})
+		c.Destroy()
+	})
+}
+
+func controllerType(action reflect.Value) (reflect.Type, error) {
+	t := action.Type()
+
+	if t.Kind() != reflect.Func {
+		return t, errors.New("Action is not a function")
+	}
+
+	if t.NumIn() != 1 {
+		return t, errors.New("Wrong Number of Arguments in action")
+	}
+
+	t = t.In(0)
+	for t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	if !t.Implements(interfaceType) {
+		return t, errors.New("Controller does not implement ctrl.Controller interface")
+	}
+
+	return t, nil
+}
+
+func interfaceOf(value interface{}) reflect.Type {
+	t := reflect.TypeOf(value)
+
+	for t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	return t
+}
---
+++ ctrl_test.go
@@ -0,0 +1,37 @@
+package ctrl
+
+import (
+	"reflect"
+	"testing"
+)
+
+type TestController struct {
+	*Base
+}
+
+func (t *TestController) Index() {
+}
+
+type NoController struct {
+}
+
+func (n *NoController) Foo() {
+}
+
+func TestValidateAction(t *testing.T) {
+	var validateTests = []struct {
+		action interface{}
+		valid  bool
+	}{
+		{(*TestController).Index, true},
+		{(*NoController).Foo, false},
+		{"bad", false},
+	}
+
+	for _, test := range validateTests {
+		_, err := controllerType(reflect.ValueOf(test.action))
+		if (err == nil) != test.valid {
+			t.Errorf("Action: %v should be valid=%v but returned error %v", reflect.ValueOf(test.action), test.valid, err)
+		}
+	}
+}
